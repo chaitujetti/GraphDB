b60f7e5440149f7450b2f63c6740d209ddd16ee8
chaitanya jetti <chaitanya@chaitanyas-MacBook-Pro.local>
2017-04-26 12:20:28 -0700

    minor fixes

--- phase2/TriangleQuery.java
+++ phase2/TriangleQuery.java
@@ -12,73 +12,70 @@ import java.io.IOException;
 public class TriangleQuery implements GlobalConst{
     FileScan iter1,iter2, iter3;
     CondExpr[] expr1,expr2;
-    AttrType[] E1Type,E2Type,E3Type,otype;
+    AttrType[] attr1,attr2,attr3,otype;
     FldSpec[] projection1,projection2;
     SortMerge s1,s2;
 
     public TriangleQuery(SystemDefs systemdef,String query) throws Exception {
-        //Extract condition expressions for queries
         String[] queries = query.split(";");
         expr1 = condExpr1(queries[0], queries[1]);
         expr2 = condExpr2(queries[2]);
 
-        E1Type = new AttrType[8];
-        E1Type[0] = new AttrType(AttrType.attrString);
-        E1Type[1] = new AttrType(AttrType.attrString);
-        E1Type[2] = new AttrType(AttrType.attrString);
-        E1Type[3] = new AttrType(AttrType.attrInteger);
-        E1Type[4] = new AttrType(AttrType.attrInteger);
-        E1Type[5] = new AttrType(AttrType.attrInteger);
-        E1Type[6] = new AttrType(AttrType.attrInteger);
-        E1Type[7] = new AttrType(AttrType.attrInteger);
+        attr1 = new AttrType[8];
+        attr1[0] = new AttrType(AttrType.attrString);
+        attr1[1] = new AttrType(AttrType.attrString);
+        attr1[2] = new AttrType(AttrType.attrString);
+        attr1[3] = new AttrType(AttrType.attrInteger);
+        attr1[4] = new AttrType(AttrType.attrInteger);
+        attr1[5] = new AttrType(AttrType.attrInteger);
+        attr1[6] = new AttrType(AttrType.attrInteger);
+        attr1[7] = new AttrType(AttrType.attrInteger);
 
-        short []E1size = new short[3];
-        E1size[0] = Tuple.LABEL_MAX_LENGTH;
-        E1size[1] = Tuple.LABEL_MAX_LENGTH;
-        E1size[2] = Tuple.LABEL_MAX_LENGTH;
+        short []size1 = new short[3];
+        size1[0] = Tuple.LABEL_MAX_LENGTH;
+        size1[1] = Tuple.LABEL_MAX_LENGTH;
+        size1[2] = Tuple.LABEL_MAX_LENGTH;
 
 
-        FldSpec []E1Proj = new FldSpec[8];
+        FldSpec []proj1 = new FldSpec[8];
         for (int i=0;i<8;i++) {
-            E1Proj[i] = new FldSpec(new RelSpec(RelSpec.outer),i+1);
+            proj1[i] = new FldSpec(new RelSpec(RelSpec.outer),i+1);
         }
 
-        FldSpec []E2Proj = new FldSpec[8];
+        FldSpec []proj2 = new FldSpec[8];
         for (int i=0;i<8;i++) {
-            E2Proj[i] = new FldSpec(new RelSpec(RelSpec.innerRel),i+1);
+            proj2[i] = new FldSpec(new RelSpec(RelSpec.innerRel),i+1);
         }
 
-        E3Type = new AttrType[8];
-        E3Type[0] = new AttrType(AttrType.attrString);
-        E3Type[1] = new AttrType(AttrType.attrString);
-        E3Type[2] = new AttrType(AttrType.attrString);
-        E3Type[3] = new AttrType(AttrType.attrInteger);
-        E3Type[4] = new AttrType(AttrType.attrString);
-        E3Type[5] = new AttrType(AttrType.attrString);
-        E3Type[6] = new AttrType(AttrType.attrString);
-        E3Type[7] = new AttrType(AttrType.attrInteger);
+        attr3 = new AttrType[8];
+        attr3[0] = new AttrType(AttrType.attrString);
+        attr3[1] = new AttrType(AttrType.attrString);
+        attr3[2] = new AttrType(AttrType.attrString);
+        attr3[3] = new AttrType(AttrType.attrInteger);
+        attr3[4] = new AttrType(AttrType.attrString);
+        attr3[5] = new AttrType(AttrType.attrString);
+        attr3[6] = new AttrType(AttrType.attrString);
+        attr3[7] = new AttrType(AttrType.attrInteger);
 
-        short []E3size = new short[6];
-        E3size[0] = Tuple.LABEL_MAX_LENGTH;
-        E3size[1] = Tuple.LABEL_MAX_LENGTH;
-        E3size[2] = Tuple.LABEL_MAX_LENGTH;
-        E3size[3] = Tuple.LABEL_MAX_LENGTH;
-        E3size[4] = Tuple.LABEL_MAX_LENGTH;
-        E3size[5] = Tuple.LABEL_MAX_LENGTH;
+        short []size2 = new short[6];
+        size2[0] = Tuple.LABEL_MAX_LENGTH;
+        size2[1] = Tuple.LABEL_MAX_LENGTH;
+        size2[2] = Tuple.LABEL_MAX_LENGTH;
+        size2[3] = Tuple.LABEL_MAX_LENGTH;
+        size2[4] = Tuple.LABEL_MAX_LENGTH;
+        size2[5] = Tuple.LABEL_MAX_LENGTH;
 
-        iter1 = new FileScan(systemdef.JavabaseDB.getEhf()._fileName,E1Type,E1size,(short)8,(short)8,E1Proj,null);
-        iter2 = new FileScan(systemdef.JavabaseDB.getEhf()._fileName,E1Type,E1size,(short)8,(short)8,E1Proj,null);
-        iter3 = new FileScan(systemdef.JavabaseDB.getEhf()._fileName,E1Type,E1size,(short)8,(short)8,E1Proj,null);
+        iter1 = new FileScan(systemdef.JavabaseDB.getEhf()._fileName,attr1,size1,(short)8,(short)8,proj1,null);
+        iter2 = new FileScan(systemdef.JavabaseDB.getEhf()._fileName,attr1,size1,(short)8,(short)8,proj1,null);
+        iter3 = new FileScan(systemdef.JavabaseDB.getEhf()._fileName,attr1,size1,(short)8,(short)8,proj1,null);
 
         TupleOrder asc = new TupleOrder(TupleOrder.Ascending);
-        Sort sort1 = new Sort(E1Type,(short)8,E1size,iter1,1,asc,10,100);
-        //Sort sort2 = new Sort(E1Type,(short)8,E1size,iter2,2,asc,10,10);
-        //Sort sort3 = new Sort(E1Type,(short)8,E1size,iter3,2,asc,10,10);
-
-        if( sort1.get_next()== null) {
-            System.out.println("sort1 is null");
-        }
+        // Sort sort1 = new Sort(attr1,(short)8,size1,iter1,1,asc,10,100);
         
+        // if( sort1.get_next()== null) {
+        //     System.out.println("sort1 is null");
+        // }
+
         projection1 = new FldSpec[8];
         projection1[0] = new FldSpec(new RelSpec(RelSpec.outer), 1);
         projection1[1] = new FldSpec(new RelSpec(RelSpec.outer), 2);
@@ -104,11 +101,11 @@ public class TriangleQuery implements GlobalConst{
         projection2[11] = new FldSpec(new RelSpec(RelSpec.innerRel), 4);
 
         //TupleOrder asc = new TupleOrder(TupleOrder.Ascending);
-        s1 = new SortMerge(E1Type,8,E1size,E1Type,8,E1size,
+        s1 = new SortMerge(attr1,8,size1,attr1,8,size1,
                 2, 10,1,10,50,iter1,iter2,
                 false, false,asc,expr1,projection1,8);
 
-        s2 = new SortMerge(E3Type,8,E3size,E1Type,8,E1size,
+        s2 = new SortMerge(attr3,8,size2,attr1,8,size1,
                 6, 10,1,10,50,s1,iter3,
                 false, false,asc,expr2,projection2,12);
 
@@ -156,13 +153,14 @@ public class TriangleQuery implements GlobalConst{
         String[] values = label1.split(":");
         expr[1] = new CondExpr();
         expr[1].next   = null;
-        expr[1].op    = new AttrOperator(AttrOperator.aopEQ);
         expr[1].type1 = new AttrType(AttrType.attrSymbol);
         if(values[0].equals("L")){
+            expr[1].op    = new AttrOperator(AttrOperator.aopEQ);
             expr[1].type2 = new AttrType(AttrType.attrString);
             expr[1].operand1.symbol = new FldSpec (new RelSpec(RelSpec.outer),3);
             expr[1].operand2.string = values[1];
         } else {
+            expr[1].op    = new AttrOperator(AttrOperator.aopLE);
             expr[1].type2 = new AttrType(AttrType.attrInteger);
             expr[1].operand1.symbol = new FldSpec (new RelSpec(RelSpec.outer),4);
             expr[1].operand2.integer = Integer.parseInt(values[1]);
@@ -171,13 +169,14 @@ public class TriangleQuery implements GlobalConst{
         values = label2.split(":");
         expr[2] = new CondExpr();
         expr[2].next   = null;
-        expr[2].op    = new AttrOperator(AttrOperator.aopEQ);
         expr[2].type1 = new AttrType(AttrType.attrSymbol);
         if(values[0].equals("L")){
+            expr[2].op    = new AttrOperator(AttrOperator.aopEQ);
             expr[2].type2 = new AttrType(AttrType.attrString);
             expr[2].operand1.symbol = new FldSpec (new RelSpec(RelSpec.innerRel),3);
             expr[2].operand2.string = values[1];
         } else {
+            expr[2].op    = new AttrOperator(AttrOperator.aopLE);
             expr[2].type2 = new AttrType(AttrType.attrInteger);
             expr[2].operand1.symbol = new FldSpec (new RelSpec(RelSpec.innerRel),4);
             expr[2].operand2.integer = Integer.parseInt(values[1]);
@@ -209,13 +208,14 @@ public class TriangleQuery implements GlobalConst{
         String[] values = label.split(":");
         expr[2] = new CondExpr();
         expr[2].next   = null;
-        expr[2].op    = new AttrOperator(AttrOperator.aopLE);
         expr[2].type1 = new AttrType(AttrType.attrSymbol);
         if(values[0].equals("L")){
+            expr[2].op    = new AttrOperator(AttrOperator.aopEQ);
             expr[2].type2 = new AttrType(AttrType.attrString);
             expr[2].operand1.symbol = new FldSpec (new RelSpec(RelSpec.innerRel),3);
             expr[2].operand2.string = values[1];
         } else {
+            expr[2].op    = new AttrOperator(AttrOperator.aopLE);
             expr[2].type2 = new AttrType(AttrType.attrInteger);
             expr[2].operand1.symbol = new FldSpec (new RelSpec(RelSpec.innerRel),4);
             expr[2].operand2.integer = Integer.parseInt(values[1]);
